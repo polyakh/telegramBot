565fde4fa7a8ea7aa5b825a3e25477356f8b9d99
polyakh <polyakh.aleksandr@gmail.com>
2023-04-20 20:29:29 +0200

    build: update webhook

--- bot/bot.mjs
+++ bot/bot.mjs
@@ -9,23 +9,25 @@ import { setupBotHandlers } from "./botHandlers.mjs";
 import { connectToDatabase } from "../db/index.mjs";
 //endregion
 
-const {TELEGRAM_BOT_TOKEN, PORT, HEROKU_APP_NAME} = process.env;
+const {TELEGRAM_BOT_TOKEN, PORT, HEROKU_APP_NAME, APP_URL} = process.env;
 const options = {
+  webHook: { port: PORT }
   // key: readFileSync('../server.key'),
   // cert: readFileSync('../server.crt')
 };
+
 
 async function startBot() {
   if (!TELEGRAM_BOT_TOKEN) {
     throw "ERROR: env variables not set.";
   }
   try {
-    const bot = new TelegramBot(TELEGRAM_BOT_TOKEN, { webHook: { port: PORT } });
+    const bot = new TelegramBot(TELEGRAM_BOT_TOKEN, options);
     await setupBotHandlers(bot);
     await connectToDatabase();
     console.log("Telegram bot started");
     const url = `https://${HEROKU_APP_NAME}.herokuapp.com:${PORT}`;
-    await bot.setWebHook(`${url}/${TELEGRAM_BOT_TOKEN}`, {
+    await bot.setWebHook(APP_URL, {
       max_connections: 1,
     });
   } catch (error) {
